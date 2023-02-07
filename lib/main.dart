f2d117db02c6af9c3ba9a10b27a627cb11be9aa7
Richie Mugambi <richiemugambi4@gmail.com>
2023-02-07 14:01:35 +0300

    in process of boxShadow

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyWidget extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        // appBar: AppBar(title: const Text('lio')),
+        // appBar: AppBar(title: Text("hi"),),
         body: MyStatefulsWidget(),
       ),
     );
@@ -50,31 +50,67 @@ class MyWidget extends StatelessWidget {
 //   }
 // }
 
+// making a banner widget
+
+// class MyStatefulsWidget extends StatelessWidget {
+//   const MyStatefulsWidget({Key? key}) : super(key: key);
+
+//   final String _title = "This is my Title";
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Container(
+//         padding: const EdgeInsets.all(10),
+//         // color: Color.fromARGB(200, 133, 55, 777),
+//         child: Banner(
+//           location: BannerLocation.topEnd,
+//           message: "new",
+//           child: Container(
+//             height: 200,
+//             width: 200,
+//             color: Color.fromARGB(255, 82, 82, 82),
+//             alignment: Alignment.center,
+//             child: const Text(
+//               "mambo",
+//               style: TextStyle(fontSize: 50, color: Colors.white),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// Box s
+
 class MyStatefulsWidget extends StatelessWidget {
   const MyStatefulsWidget({Key? key}) : super(key: key);
-
-  final String _title = "This is my Title";
-
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        // color: Color.fromARGB(200, 133, 55, 777),
-        child: Banner(
-          location: BannerLocation.topEnd,
-          message: "new",
-          child: Container(
-            height: 200,
-            width: 200,
-            color: Color.fromARGB(255, 82, 82, 82),
-            alignment: Alignment.center,
-            child: const Text(
-              "mambo",
-              style: TextStyle(fontSize: 50, color: Colors.white),
-            ),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 20,
           ),
-        ),
+          Container(
+            height: 150,
+            width: 150,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 5,
+                      spreadRadius: 1,
+                      offset: Offset(4, 4)),
+                ],
+                color: Colors.red),
+            child: const Text("Container1"),
+          )
+        ],
       ),
     );
   }
